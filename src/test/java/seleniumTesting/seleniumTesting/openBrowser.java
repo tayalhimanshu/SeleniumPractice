970647364a617e5b3d8c4455ff289048adeb3098
tayalhimanshu <tayal.1991@gmail.com>
2024-10-05 22:04:59 +0530

    first commit

---
+++ src/test/java/seleniumTesting/seleniumTesting/openBrowser.java
@@ -0,0 +1,10 @@
+package seleniumTesting.seleniumTesting;
+
+import org.testng.annotations.Test;
+
+public class openBrowser {
+	@Test
+	public void testing() {
+		System.out.println("Test");
+	}
+}
